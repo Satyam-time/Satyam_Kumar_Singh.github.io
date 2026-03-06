9afb7a900c6fcbc8744f5a3bb9296a966ad50e38
Satyam Kumar Singh <85268694+Satyam-time@users.noreply.github.com>
2026-03-06 21:25:03 +0530

    Add smooth scroll to projects section on button click

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".main-btn");
+
+button.addEventListener("click", function(){
+
+document.querySelector("#projects").scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
